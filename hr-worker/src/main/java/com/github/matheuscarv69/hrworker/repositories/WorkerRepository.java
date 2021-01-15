930a7ab944a34ba94e5051da95abc2b86cd3cc73
matheuscarv69 <matheus9126@gmail.com>
2021-01-15 16:30:51 -0400

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/github/matheuscarv69/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.github.matheuscarv69.hrworker.repositories;
+
+import com.github.matheuscarv69.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+
+}
---
+++ hr-worker/src/main/java/com/github/matheuscarv69/hrworker/resources/WorkerResource.java
@@ -0,0 +1,35 @@
+package com.github.matheuscarv69.hrworker.resources;
+
+import com.github.matheuscarv69.hrworker.entities.Worker;
+import com.github.matheuscarv69.hrworker.repositories.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResource {
+
+    @Autowired
+    private WorkerRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll() {
+        List<Worker> list = repository.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        Optional<Worker> obj = repository.findById(id);
+
+        return ResponseEntity.ok().body(obj.get());
+    }
+}
