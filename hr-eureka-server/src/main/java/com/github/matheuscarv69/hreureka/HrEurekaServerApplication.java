3759fb14daf051fc6dfdd3df73a25d4f857d9641
matheuscarv69 <matheus9126@gmail.com>
2021-01-26 10:24:37 -0400

    :wrench: feat: Configuring Eureka Server

--- hr-eureka-server/src/main/java/com/github/matheuscarv69/hreureka/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/com/github/matheuscarv69/hreureka/HrEurekaServerApplication.java
@@ -2,12 +2,14 @@ package com.github.matheuscarv69.hreureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrEurekaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HrEurekaServerApplication.class, args);
+    }
 
 }
