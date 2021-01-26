8c4108b101a6323d3c23258f0566eb79e65a1c00
matheuscarv69 <matheus9126@gmail.com>
2021-01-26 10:11:20 -0400

    :sparkles: feat: Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/github/matheuscarv69/hreureka/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matheuscarv69.hreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
