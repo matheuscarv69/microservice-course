e68b0cb80f8df369855c7547a7bfa36eba7fd771
matheuscarv69 <matheus9126@gmail.com>
2021-01-28 18:14:43 -0400

    :sparkles: feat: Project hr-user created

---
+++ hr-user/src/test/java/com/github/matheuscarv69/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matheuscarv69.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
