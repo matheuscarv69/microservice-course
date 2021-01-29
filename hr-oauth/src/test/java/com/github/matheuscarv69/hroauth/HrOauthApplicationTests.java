c533c42429fcd2fccc59759356f38299289e38f7
matheuscarv69 <matheus9126@gmail.com>
2021-01-28 23:53:17 -0400

    :wrench: feat: Security configs to config server

---
+++ hr-oauth/src/test/java/com/github/matheuscarv69/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matheuscarv69.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
